5172db2829ef6b049b3a83ae162c3e2670ba756c
C4RR0T02 <66990228+C4RR0T02@users.noreply.github.com>
2022-04-10 14:23:41 +0800

    Create Mathematical-and-Logical-operators.py

---
+++ Mathematical-and-Logical-operators.py
@@ -0,0 +1,42 @@
+# Mathematical and logical operators in python
+print (10 + 20)
+print (20 - 10)
+print (20 * 10)
+print (20 / 10) # Division always gives a float
+
+# Exponent
+print (10 ** 20)
+
+# Modulos - operations that gives you the remainder of a division
+print (20 % 2) # 0 because no remainder
+print (20 % 3) # 2 because 2 is the remainder
+
+# Logical operations
+# 10 > 3 is True
+# 3 > 10 is False
+# 3 > 3 is False
+
+# > More than
+# >= More than OR equals to
+# < Less than
+# <= Less than OR equals to 
+# == equals to 
+# != not equals to 
+
+# AND / OR / NOT  Function
+
+# AND
+# True and True is True
+# True and False is False
+# False and True is False
+# False and False is False
+
+# OR
+# True or True is True
+# True or False is True
+# False or True is True
+# False or False is False
+
+# NOT
+# not True is False
+# not False is True
