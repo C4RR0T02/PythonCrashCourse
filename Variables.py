b7ae11e1cb976028a761f1c59615688e8bc0ddc3
C4RR0T02 <66990228+C4RR0T02@users.noreply.github.com>
2022-04-10 14:24:55 +0800

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,21 @@
+# Variables
+# remember algebra
+# teacher says x = 1, so what is 2x?
+# you will say ah ok it's 2 because x + x = 1 + 1 = 2
+
+# no need to use var/let/const
+
+# Variables have no quotation marks
+name = "your name"
+print(name) # your name
+
+age = 20 # your age
+print(age) # (remember no quotation marks for integer)
+
+print(age > 18)
+print(name == "your name")
+
+# Imagine a reservation is made at a bar
+# The bar has a robot
+# robot checks for age, and identity
+print (age >= 18 and name == "your name")
