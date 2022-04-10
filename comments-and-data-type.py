3ddc9e348c28eca7a5826974a454ef4c0c2c4445
C4RR0T02 <66990228+C4RR0T02@users.noreply.github.com>
2022-04-10 14:22:30 +0800

    Create comments-and-data-type.py

---
+++ comments-and-data-type.py
@@ -0,0 +1,7 @@
+# Comments
+print ("Hello World!")
+
+# string (string) -> Sentences
+# integer (int) -> Whole Numbers
+# floats (float) -> Numbers with decimal places
+# boolean (bool) -> True/False
